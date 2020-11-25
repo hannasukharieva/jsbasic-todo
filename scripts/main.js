1fb6de949c8793e7863fe08a539f70c181799ba5
hannasukharieva <annsukhareva33@gmail.com>
2020-11-25 14:39:32 +0000

    added submit on enter

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,17 @@
 let input = document.querySelector('.input input');
 let button = document.querySelector('.input button');
 let list = document.querySelector('.list ol');
-let inputValue = '';
 
-input.addEventListener('change', handleInputChange);
+input.addEventListener('keydown', handleEnterClick);
 button.addEventListener('click', handleSubmit);
-
-
-function handleInputChange(event) {
-  inputValue = event.target.value;
-}
 
 function handleSubmit() {
   let toDoItem = document.createElement('li');
-  toDoItem.textContent = inputValue;
-  console.log(toDoItem);
+  toDoItem.textContent = input.value;
   list.appendChild(toDoItem);
   input.value = '';
-  inputValue = '';
 }
 
+function handleEnterClick(event) {
+  event.code === 'Enter' ? handleSubmit() : console.log(event.code);
+}
