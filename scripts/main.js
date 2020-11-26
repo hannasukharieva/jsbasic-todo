b3c4d9596274b85154c69770b2c61e7626944bcb
hannasukharieva <annsukhareva33@gmail.com>
2020-11-26 13:37:46 +0000

    refactor functions to arrow functions

--- scripts/main.js
+++ scripts/main.js
@@ -2,16 +2,16 @@ let input = document.querySelector('.input input');
 let button = document.querySelector('.input button');
 let list = document.querySelector('.list ol');
 
-input.addEventListener('keydown', handleEnterClick);
-button.addEventListener('click', handleSubmit);
-
-function handleSubmit() {
+let handleSubmit = () => { 
   let toDoItem = document.createElement('li');
   toDoItem.textContent = input.value;
   list.appendChild(toDoItem);
   input.value = '';
 }
 
-function handleEnterClick(event) {
+let handleEnterClick = (event) => {
   event.code === 'Enter' ? handleSubmit() : console.log(event.code);
 }
+
+input.addEventListener('keydown', handleEnterClick);
+button.addEventListener('click', handleSubmit);
